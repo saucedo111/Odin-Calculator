41af3630806b77b38d2cc3ed2f1ad3ea88ae0b99
saucedo111 <99573171+saucedo111@users.noreply.github.com>
2023-10-06 15:18:36 -0400

    basic logic finished, TODO add multi-operation support and color

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let display = document.querySelector('.display');
+let displayValue = '';
+let buttons = document.querySelectorAll('button');
+
 function add(a, b) {
     return a + b;
 }
@@ -11,14 +15,14 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if(b = 0) {
+    if (b === 0) {
         return "Error: Cannot divide by zero";
     }
     return a / b;
 }
 
 function operate(operator, a, b) {
-    switch(operator) {
+    switch (operator) {
         case '+':
             return add(a, b);
         case '-':
@@ -29,3 +33,52 @@ function operate(operator, a, b) {
             return divide(a, b);
     }
 }
+
+function compute(str) {
+    let num1 = '';
+    let num2 = '';
+    let op = '';
+    let flag = false;
+    for (let char of str) {
+        if (char === '+' || char === '-' || char === '*' || char === '/') {
+            op = char;
+            flag = true;
+        } else {
+            if (flag === false) {
+                num1 += char;
+            } else {
+                num2 += char;
+            }
+        }
+    }
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return operate(op, num1, num2);
+
+}
+
+function updateDisplay() {
+    display.textContent = displayValue;
+}
+
+buttons.forEach((button) => {
+    if (button.id !== 'clear' && button.id !== '=') {
+        button.addEventListener('click', () => {
+            displayValue = displayValue + button.textContent;
+            updateDisplay();
+        })
+    }
+    if (button.id === 'clear') {
+        button.addEventListener('click', () => {
+            displayValue = '';
+            updateDisplay();
+        })
+    }
+    if (button.id === '=') {
+        button.addEventListener('click', () => {
+            displayValue = compute(displayValue);
+            updateDisplay();
+        })
+    }
+})
+
